e0323e43a8afe975fafcf719629a171a54cc565f
David Almeida <davidalmeidadrive@gmail.com>
2020-03-13 10:26:20 -0300

    Injeção de dependencia

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -8,9 +8,9 @@ public class RentalService {
 	private Double pricePerDay;
 	private Double pricePerHour;
 
-	private BrazilTaxService taxService;
+	private TaxService taxService;
 
-	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+	public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
 		this.pricePerDay = pricePerDay;
 		this.pricePerHour = pricePerHour;
 		this.taxService = taxService;
@@ -39,7 +39,7 @@ public class RentalService {
 		}
 		else {
 			basicPayment = pricePerDay * Math.ceil(hours / 24);
-		}
+		} 
 		
 		double tax = taxService.tax(basicPayment);
 		
